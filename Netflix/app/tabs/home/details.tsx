aeae4cb208ea1b6c35f180265e87c2a86e7a0258
FranciscoM2807 <frm2807@gmail.com>
2024-09-19 07:39:21 -0300

    Netflix

---
+++ Netflix/app/tabs/home/details.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native'
+import React from 'react'
+
+const Details: React.FC = () => {
+    return (
+        <View>
+            <Text>
+                Detail Screen
+            </Text>
+        </View>
+    )
+}
+
+export default Details
---
+++ Netflix/config/fonts.ts
@@ -0,0 +1,7 @@
+export default {
+    "poppins-regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "poppins-medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "poppins-bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "poppins-semiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+
+};
